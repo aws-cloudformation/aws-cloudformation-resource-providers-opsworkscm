a5e61f20ae72ad040d56796b11acccfb5673e706
Bernard Jollans <jollans@amazon.com>
2020-04-27 08:55:03 +0000

    Fixing OpsWorksCMClient creation

--- aws-opsworkscm-server/src/main/java/software/amazon/opsworkscm/server/ClientBuilder.java
+++ aws-opsworkscm-server/src/main/java/software/amazon/opsworkscm/server/ClientBuilder.java
@@ -2,13 +2,18 @@ package software.amazon.opsworkscm.server;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.opsworkscm.OpsWorksCmClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientBuilder {
-    static OpsWorksCmClient getClient() {
+
+    static OpsWorksCmClient getClient(final String region) {
         return OpsWorksCmClient.builder()
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .region(Region.of(region))
                 .httpClient(LambdaWrapper.HTTP_CLIENT)
                 .build();
     }
