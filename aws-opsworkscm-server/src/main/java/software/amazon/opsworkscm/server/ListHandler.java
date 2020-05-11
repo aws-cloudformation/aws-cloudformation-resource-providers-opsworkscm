9dad93859d541a23f0ee4080c5255fc129edc39e
Bernard Jollans <Nopx@users.noreply.github.com>
2020-05-11 13:17:26 +0200

    Add ListHandler with UnitTests (#11)

--- aws-opsworkscm-server/src/main/java/software/amazon/opsworkscm/server/ListHandler.java
+++ aws-opsworkscm-server/src/main/java/software/amazon/opsworkscm/server/ListHandler.java
@@ -1,9 +1,12 @@
 package software.amazon.opsworkscm.server;
 
+import software.amazon.awssdk.services.opsworkscm.OpsWorksCmClient;
+import software.amazon.awssdk.services.opsworkscm.model.DescribeServersResponse;
+import software.amazon.awssdk.services.opsworkscm.model.Server;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
-import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
 import java.util.ArrayList;
@@ -11,20 +14,47 @@ import java.util.List;
 
 public class ListHandler extends BaseHandler<CallbackContext> {
 
+    private static final int NO_CALLBACK_DELAY = 0;
+
+    CallbackContext callbackContext;
+    Logger logger;
+    ResourceHandlerRequest<ResourceModel> request;
+    ClientWrapper client;
+
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final Logger logger) {
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final Logger logger) {
 
-        final List<ResourceModel> models = new ArrayList<>();
+        this.logger = logger;
 
-        // TODO : put your code here
+        final OpsWorksCmClient opsWorksCmClientclient = ClientBuilder.getClient();
+        this.client = new ClientWrapper(opsWorksCmClientclient, request.getDesiredResourceState(), request.getPreviousResourceState(), proxy, logger);
 
+        logger.log("Calling Describe Servers with no ServerName");
+
+        DescribeServersResponse result = client.describeAllServers();
+        if (result == null || result.servers() == null) {
+            logger.log("Describe result is Null. Retrying request.");
+            return ProgressEvent.defaultInProgressHandler(callbackContext, NO_CALLBACK_DELAY, request.getDesiredResourceState());
+        }
+
+        List<ResourceModel> models = addDescribeServersResponseAttributes(result);
         return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModels(models)
-            .status(OperationStatus.SUCCESS)
-            .build();
+                .resourceModels(models)
+                .status(OperationStatus.SUCCESS)
+                .build();
+    }
+
+    private List<ResourceModel> addDescribeServersResponseAttributes(final DescribeServersResponse response) {
+        List<ResourceModel> models = new ArrayList<>();
+        List<Server> servers = response.hasServers() ? response.servers() : new ArrayList<>();
+        servers.forEach(server -> models.add(ResourceModel.builder()
+                .endpoint(server.endpoint())
+                .serverName(server.serverName())
+                .build()));
+        return models;
     }
 }
